639482064fde8a895e07d324952e31a6186b648f
DANIL <longstmia@yandex.ru>
2024-12-06 21:48:41 +0300

    06.12

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// Стилизованный компонент для Footer
 const FooterContainer = styled.footer`
-    position: fixed; /* Фиксируем футер внизу страницы */
+    position: fixed; 
     bottom: 0;
     left: 0;
     width: 100%;
     height: 88px;
-    background-color: #000000; /* Цвет фона */
-    border-top-left-radius: 20px; /* Закругленные углы */
+    background-color: #000000; 
+    border-top-left-radius: 20px; 
     border-top-right-radius: 20px;
     display: flex;
-    justify-content: center; /* Центрируем кнопки по горизонтали */
-    align-items: center; /* Центрируем кнопки по вертикали */
-    box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.2); /* Тень для эффекта */
+    justify-content: center; 
+    align-items: center; 
+    box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.2); 
 `;
 
 
